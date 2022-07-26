a65764915400ce2c053f693a9739661dc5a3894a
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-07-25 19:44:50 -0700

    done

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,14 @@
+class SightingsController < ApplicationController
+    def index
+        sightings = Sighting.all
+        redner json: sightings, include: [:bird, :location]
+    end
+    def show
+        sighting = Sighting.find_by(id: params[:id])
+        if sighting
+            render json: sighting, include: [:bird, :location], except: [:updated_at]
+        else
+            render json: {message: 'No sighting found with that id'}
+        end
+    end
+end
